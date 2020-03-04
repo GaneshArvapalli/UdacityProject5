485e6a512e2f0f16e6656a0216c24c0304ce5d85
root <ganesh.arvapalli@gmail.com>
2020-03-04 20:47:42 +0000

    Subscriber added, now compiles

--- catkin_ws/src/add_markers/src/add_markers.cpp
+++ catkin_ws/src/add_markers/src/add_markers.cpp
@@ -1,5 +1,14 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
+#include <nav_msgs/Odometry.h>
+
+void robot_state_callback(const nav_msgs::Odometry::ConstPtr& msg) {
+	ROS_INFO("Position-> x: [%f], y: [%f], z: [%f]", msg->pose.pose.position.x,msg->pose.pose.position.y, msg->pose.pose.position.z);
+	ROS_INFO("Orientation-> x: [%f], y: [%f], z: [%f], w: [%f]", msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, 
+             msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
+	
+}
+
 
 int main( int argc, char** argv )
 {
@@ -8,6 +17,8 @@ int main( int argc, char** argv )
   ros::Rate r(1);
   ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
 
+  ros::Subscriber odom_sub = n.subscribe("/odom", 10, robot_state_callback);
+  
   // Set our initial shape type to be a cube
   uint32_t shape = visualization_msgs::Marker::CUBE;
 
