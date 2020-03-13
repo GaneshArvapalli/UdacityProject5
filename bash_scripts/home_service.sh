b6073034525268d3cbcd72cac2807998bd060ce8
root <ganesh.arvapalli@gmail.com>
2020-03-13 15:20:08 +0000

    Moved scripts to separate folder, fixed add_marker message generation

---
+++ bash_scripts/home_service.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# xterm  -e  " source /opt/ros/kinetic/setup.bash; roscore" & 
+# sleep 2
+xterm  -e  " source ../catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" & 
+sleep 5
+xterm  -e  " source ../catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" & 
+sleep 5
+xterm  -e  " source ../catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" & 
+sleep 5
+xterm  -e  " source ../catkin_ws/devel/setup.bash; roslaunch add_markers add_markers_demo.launch" & 
+sleep 1
+xterm  -e  " source ../catkin_ws/devel/setup.bash; roslaunch pick_objects pick_objects_demo.launch" & 
+sleep 1
--- test_navigation.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-xterm  -e  " source /opt/ros/kinetic/setup.bash; roscore" & 
-sleep 5
-xterm  -e  " source /home/workspace/UdacityProject5/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" & 
-sleep 5
-xterm  -e  " source /home/workspace/UdacityProject5/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" & 
-sleep 5
-xterm  -e  " source /home/workspace/UdacityProject5/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" & 
-sleep 5
